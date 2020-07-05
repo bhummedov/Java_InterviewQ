e5ea668c82a6d91c35cd0dccb34c1a9cf0354041
Berdi <bhummedov@na.edu>
2020-07-05 12:21:08 -0500

    Berdi's Java Codes

---
+++ src/OCA/OCA.java
@@ -0,0 +1,9 @@
+package OCA;
+
+public class OCA {
+    public static void main(String[] args) {
+        Float d= 0f;
+        System.out.print(d.BYTES);
+        System.out.print(d.SIZE);
+    }
+}
